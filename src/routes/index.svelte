5b0b13746a718e8b3d400c9a02a13423095dcad2
Aaron Long <aaronlong0712@gmail.com>
2022-07-05 00:23:07 -0400

    Ready

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,61 +1,128 @@
-<script context="module">
+<!-- <script context="module">
 	export const prerender = true;
-</script>
+</script> -->
 
 <script>
-	import Counter from '$lib/Counter.svelte';
+	async function handleClick() {
+		const webhookBody = {
+          embeds: [{
+            title: 'Suggestion Submitted',
+            fields: [
+              { name: 'Roblox User', value: document.getElementById("user").value },
+              { name: 'Suggestion Name', value: document.getElementById("name").value },
+			  { name: 'Suggestion Description', value: document.getElementById("desc").value }
+            ]
+          }],
+        };
+
+        const webhookUrl = 'https://discord.com/api/webhooks/993682825041674260/WmMff1GkcNcptsZ5YQ84KbUQ18kAX1ezszzBSjIrdX5GgXXyymg0HR-mtTHdi59kG35S';
+
+        const response = await fetch(webhookUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(webhookBody),
+        });
+
+        if (response.ok) {
+		  document.getElementById("user").value = "";
+		  document.getElementById("name").value = "";
+		  document.getElementById("desc").value = "";
+		  document.getElementById("subForm").style.display = "none"
+		  document.getElementById("subDiv").style.display = "none"
+		  document.getElementById("completed").style.display = "flex"
+		  setTimeout(() => {
+			document.getElementById("completed").style.display = "none"
+			document.getElementById("subForm").style.display = "flex"
+		    document.getElementById("subDiv").style.display = "flex"
+		  }, 5000)
+        } else {
+          alert('There was an error! Try again later!');
+        };
+	}
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Bulk Games Area 27" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+	<form id="subForm">
+		<label for="name">ROBlOX USERNAME:</label><br>
+		<input class="name" type="text" id="user" name="user"><br>
+		<label for="name">SUGGESTION NAME:</label><br>
+		<input class="name" type="text" id="name" name="name"><br>
+		<label for="desc">DESCRIPTION:</label><br>
+		<textarea class="desc" id="desc" name="desc" rows="4" cols="50" ></textarea><br>
+	</form>
+	<div id="subDiv">
+		<button on:click={handleClick}>
+			SUBMIT
+		</button>
+	</div>
+	<div class="completed" id="completed">
+		<p>Suggestion Sent</p>
+	</div>
 </section>
 
 <style>
-	section {
+
+	form {
 		display: flex;
 		flex-direction: column;
+		padding-left: 10%;
+		padding-right: 10%;
+	}
+
+	label {
+		font-size: 26px;
+		font-weight: 700;
+	}
+
+	input.name {
+		width: 100%;
+		border-radius: 5px;
+		border: 2px solid rgb(0,117,176);
+	}
+
+	textarea.desc {
+		width: 100%;
+		height: 30vh;
+		border-radius: 5px;
+		border: 2px solid rgb(0,117,176);
+		font-size: 18px;
+	}
+
+	div {
+		display: flex;
 		justify-content: center;
-		align-items: center;
-		flex: 1;
+		width:100%;
 	}
 
-	h1 {
-		width: 100%;
+	div.completed {
+		display: none;
+		height: 100vh;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+	p {
+		font-weight: 700;
+		font-size: 30px;
+	}
+	button {
+		width: 20%;
+		border-radius: 5px;
+		color: white;
+		font-weight: 700;
+		background-color: rgb(0,117,176);
+		border: 3px solid rgb(0,117,176);
+		transition: all 0.5s ease;
 	}
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	button:hover {
+		background-color: rgb(255,255,255);
+		color: black;
 	}
+
 </style>
